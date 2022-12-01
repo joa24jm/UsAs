3f5a402c6434babb3976e1f5f33482c188cd50a1
johannesallgaier <johannes.allgaier@uni-wuerzburg.de>
2022-12-01 13:37:57 +0100

    fix typos in process functions

--- src/d00_utils/process_uniti_dataset.py
+++ src/d00_utils/process_uniti_dataset.py
@@ -8,14 +8,13 @@ def load_uniti_dataset(file_path: str = "../../data/d01_raw/uniti/uniti_dataset_
     df = pd.read_csv(file_path)
     df = helpers.create_target_shift(df, target_name=target_col)
     df.sort_values(by=[timestamp_col, user_id_col], inplace=True)
-    df = df.reset_index().rename({'index':'answer_id'}, axis=1)
+    df = df.reset_index().rename({'index': 'answer_id'}, axis=1)
     return df
 
 
 def main():
     uniti_df = load_uniti_dataset()
-    uniti_df.to_csv("data/d02_processed/uniti.csv", index=False)
-
+    uniti_df.to_csv("../../data/d02_processed/uniti.csv", index=False)
 
 if __name__ == "__main__":
     main()
